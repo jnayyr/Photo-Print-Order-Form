d6e0f870350d4362b8e0b4ff321afb5cfc5781c6
Ajay Nair <AJ4942BO@go.minnstate.edu>
2020-04-15 13:56:27 -0500

    Commit #4.

--- Photo Print Order Form/Form1.Designer.cs
+++ Photo Print Order Form/Form1.Designer.cs
@@ -28,13 +28,103 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.cbxSize = new System.Windows.Forms.ComboBox();
+            this.btnCalculate = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtPrice = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtQuantity = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(81, 46);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(120, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Select Size";
+            // 
+            // cbxSize
+            // 
+            this.cbxSize.FormattingEnabled = true;
+            this.cbxSize.Location = new System.Drawing.Point(255, 38);
+            this.cbxSize.Name = "cbxSize";
+            this.cbxSize.Size = new System.Drawing.Size(187, 33);
+            this.cbxSize.TabIndex = 1;
+            // 
+            // btnCalculate
+            // 
+            this.btnCalculate.Location = new System.Drawing.Point(153, 150);
+            this.btnCalculate.Name = "btnCalculate";
+            this.btnCalculate.Size = new System.Drawing.Size(193, 51);
+            this.btnCalculate.TabIndex = 2;
+            this.btnCalculate.Text = "Calculate Price";
+            this.btnCalculate.UseVisualStyleBackColor = true;
+            this.btnCalculate.Click += new System.EventHandler(this.btnCalculate_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(121, 227);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(61, 25);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Price";
+            // 
+            // txtPrice
+            // 
+            this.txtPrice.Location = new System.Drawing.Point(255, 227);
+            this.txtPrice.Name = "txtPrice";
+            this.txtPrice.Size = new System.Drawing.Size(187, 31);
+            this.txtPrice.TabIndex = 4;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(109, 100);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(92, 25);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Quantity";
+            // 
+            // txtQuantity
+            // 
+            this.txtQuantity.Location = new System.Drawing.Point(255, 100);
+            this.txtQuantity.Name = "txtQuantity";
+            this.txtQuantity.Size = new System.Drawing.Size(187, 31);
+            this.txtQuantity.TabIndex = 6;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(546, 314);
+            this.Controls.Add(this.txtQuantity);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtPrice);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btnCalculate);
+            this.Controls.Add(this.cbxSize);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Photo Order";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox cbxSize;
+        private System.Windows.Forms.Button btnCalculate;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtPrice;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtQuantity;
     }
 }
 
--- Photo Print Order Form/Form1.cs
+++ Photo Print Order Form/Form1.cs
@@ -16,5 +16,54 @@ namespace Photo_Print_Order_Form
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            cbxSize.Items.Add("Small");
+            cbxSize.Items.Add("Medium");
+            cbxSize.Items.Add("Large");
+            cbxSize.Items.Add("Extra Large");
+
+            cbxSize.SelectedIndex = 0; // Select the first size
+        }
+
+        private void btnCalculate_Click(object sender, EventArgs e)
+        {
+            if(Int32.TryParse(txtQuantity.Text, out int quantity) == false)
+            {
+                MessageBox.Show("Quantity must be a number", "Error");
+                return;
+            }
+            if (quantity < 1) // Making sure quantity is positive
+            {
+                MessageBox.Show("Quantity must be positive", "Error");
+            }
+                
+            string size = cbxSize.SelectedItem.ToString();
+
+            double price;
+
+            switch(size)
+            {
+                case "Small":
+                    price = 0.20;
+                    break;
+                case "Medium":
+                    price = 0.30;
+                    break;
+                case "Large":
+                    price = 0.40;
+                    break;
+                case "Extra Large":
+                    price = 0.50;
+                    break;
+                default:
+                    MessageBox.Show("Unknown size");
+                    return; // stop processing this event
+            }
+
+            double total = price * quantity;
+            txtPrice.Text = total.ToString("c"); // Format as currency
+        }
     }
 }
